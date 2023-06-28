51113ead7796070b09294386a3c9df728be5509c
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-28 23:07:18 +0530

    Implemented RUD

--- api/user.php
+++ api/user.php
@@ -7,17 +7,41 @@ include 'db_conf.php';
 $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case "GET":
-        $stmt = $pdo->query("SELECT * FROM user ");
-        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        if ($rows > 0) {
-            foreach ($rows as $row) {
-                $json_arr["userdata"][] = array("id" => $row['id'], "username" => $row['username'], "email" => $row['email'], "mobile" => $row['mobile']);
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        if (isset($path[4]) && is_numeric($path[4])) {
+            $id = $path[4]; // Assuming $path is an array with the desired value at index 4
+
+            // Prepare and execute the SQL statement
+            $stmt = $pdo->prepare("SELECT * FROM user WHERE id = :id");
+            $stmt->execute([
+                'id' => $id
+            ]);
+
+            // Fetch the results
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $json_arr = array();
+            // Output the results (for demonstration purposes)
+            if ($rows > 0) {
+                foreach ($rows as $row) {
+                    $json_arr['userData'] = array('id' => $row['id'], 'email' => $row['email'], 'username' => $row['username'], 'mobile' => $row['mobile']);
+                }
+                echo json_encode($json_arr["userData"]);
+                return;
             }
-            echo json_encode($json_arr["userdata"]);
-            return;
         } else {
-            echo json_encode(["result" => "Please Check The Data"]);
-            return;
+            $stmt = $pdo->query("SELECT * FROM user ");
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $json_arr = array();
+            if (count($rows) > 0) {
+                foreach ($rows as $row) {
+                    $json_arr["userdata"][] = array("id" => $row['id'], "username" => $row['username'], "email" => $row['email'], "mobile" => $row['mobile']);
+                }
+                echo json_encode($json_arr["userdata"]);
+                return;
+            } else {
+                echo json_encode(["result" => "No Users Available"]);
+                return;
+            }
         }
         break;
     case "POST":
@@ -38,6 +62,46 @@ switch ($method) {
         } else {
             echo json_encode(["success" => "Insertion Failed"]);
             return;
+        }
+        break;
+    case "PUT":
+        $userData = json_decode(file_get_contents("php://input"));
+        $id = $userData->id;
+        $username = $userData->username;
+        $email = $userData->email;
+        $mobile = $userData->mobile;
+        $stmt = $pdo->prepare("UPDATE user SET username = :username, email = :email, mobile = :mobile WHERE id = :id");
+        $result = $stmt->execute([
+            'username' => strtoupper($username),
+            'email' => $email,
+            'mobile' => $mobile,
+            'id' => $id
+        ]);
+        if ($result) {
+            echo json_encode(["success" => "Updated Successfully"]);
+            return;
+        } else {
+            echo json_encode(["success" => "Updation Failed"]);
+            return;
+        }
+        break;
+    case "DELETE":
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+        if (isset($path[4]) && is_numeric($path[4])) {
+            $id = $path[4]; // Assuming $path is an array with the desired value at index 4
+
+            // Prepare and execute the SQL statement
+            $stmt = $pdo->prepare("DELETE FROM user WHERE id = :id");
+            $result = $stmt->execute([
+                'id' => $id
+            ]);
+            if ($result) {
+                echo json_encode(["success" => "Deletd Successfully"]);
+                return;
+            } else {
+                echo json_encode(["success" => "Deletion Failed"]);
+                return;
+            }
         }
         break;
 }
